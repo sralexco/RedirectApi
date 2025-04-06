d71e86a2ebed8aa00d1453ba6c0a924c191825ca
alex <alex@alexs-Mac-mini.local>
2025-04-06 08:20:25 -0500

    First Commit

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  const code = req.query.code;
+  if (code) {
+    res.redirect('InstagramProject://oauth-callback/instagram?code=${encodeURIComponent(code)}');
+  } else {
+    res.send('Missing code.');
+  }
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on port ${port}`));
