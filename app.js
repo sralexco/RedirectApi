b46d9920c0eaf46fe9fa391949ee316e35b257f1
alex <alex@alexs-Mac-mini.local>
2025-04-06 12:09:44 -0500

    change of port

--- app.js
+++ app.js
@@ -10,5 +10,5 @@ app.get('/', (req, res) => {
   }
 });
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 app.listen(port, () => console.log(`Listening on port ${port}`));
